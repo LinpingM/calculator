65aaa82c8d494593018872d434320f44d2ba7879
Kirill <kirmail04@gmail.com>
2020-11-27 13:52:08 +0300

    Update calculator.py
    fix bug, add func is_digit()

--- calculator.py
+++ calculator.py
@@ -16,24 +16,32 @@ BUTTONS = (
 )
 
 
+def is_digit(string):
+    if string.isdigit():
+       return True
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
 def instantOperations(text):
     labelValue = label.cget('text')
-    if labelValue == '':
-        labelValue = '0'
-    if text == '%':
-        labelValue = float(labelValue) / 100
-    elif text == '√' and float(labelValue) >= 0:
-        labelValue = float(labelValue) ** 0.5
-    elif text == 'x²':
-        labelValue = float(labelValue) ** 2
-    elif text == '¹/x' and float(labelValue) != 0:
-        labelValue = 1 / float(labelValue)
-    elif text == 'сᴇ' or text == 'с':
-        labelValue = 0
-    elif text == '<':
+    if is_digit(labelValue):
+        if text == '%':
+            labelValue = float(labelValue) / 100
+        elif text == '√' and float(labelValue) >= 0:
+            labelValue = float(labelValue) ** 0.5
+        elif text == 'x²':
+            labelValue = float(labelValue) ** 2
+        elif text == '¹/x' and float(labelValue) != 0:
+            labelValue = 1 / float(labelValue)
+        elif text == 'сᴇ' or text == 'с':
+            labelValue = 0
+        elif text == '±':
+            labelValue = -float(labelValue)
+    if text == '<':
         labelValue = labelValue[:-1]
-    elif text == '±':
-        labelValue = -float(labelValue)
     label['text'] = str(labelValue)
 
 def numbers(text):
@@ -65,7 +73,8 @@ screenH = root.winfo_screenheight()
 # root's coordinates
 root.x = int(screenW/2 - root.width/2)
 root.y = int(screenH/2 - root.height/2)
-root.geometry(f'{root.width}x{root.height}+{root.x}+{root.y}')
+#root.geometry(f'{root.width}x{root.height}+{root.x}+{root.y}')
+root.geometry(f'+{root.x}+{root.y}')
 
 
 label = Label(text='0', font='Arial 28', bg=BGROOT, height=3)
